b68c5aec31ce4b96b703656ae0e818ffbb8de39b
kallurishyamsundhar <85303475+kallurishyamsundhar@users.noreply.github.com>
2023-11-08 12:15:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+// var mydrums=document.querySelectorAll(".drum").length;
+// for (var i=0;i<mydrums;i++){
+//     document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+//         // alert("hey you clicked me ");
+//         var audio=new Audio("sounds/crash.mp3");
+//         audio.play();
+//     });
+
+// }
+document.querySelector(".w").addEventListener("click",function(){
+    var audio=new Audio("sounds/1.mp3");
+           audio.play();
+});
+document.querySelector(".a").addEventListener("click",function(){
+    var audio=new Audio("sounds/2.mp3");
+           audio.play();
+});
+document.querySelector(".s").addEventListener("click",function(){
+    var audio=new Audio("sounds/3.mp3");
+           audio.play();
+});
+document.querySelector(".d").addEventListener("click",function(){
+    var audio=new Audio("sounds/4.mp3");
+           audio.play();
+});
+document.querySelector(".j").addEventListener("click",function(){
+    var audio=new Audio("sounds/5.mp3");
+           audio.play();
+});
+document.querySelector(".k").addEventListener("click",function(){
+    var audio=new Audio("sounds/6.mp3");
+           audio.play();
+});
+document.querySelector(".l").addEventListener("click",function(){
+    var audio=new Audio("sounds/7.mp3");
+           audio.play();
+});
+
